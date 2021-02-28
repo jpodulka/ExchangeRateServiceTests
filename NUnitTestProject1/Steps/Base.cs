fec9a9b88d61834a48df42a59d2992ef370361ea
Jedrzej Podulka <jpodulka@gmail.com>
2021-02-28 21:07:17 +0100

    improve

---
+++ NUnitTestProject1/Steps/Base.cs
@@ -0,0 +1,33 @@
+﻿using NUnitTestProject1.Models;
+using RestSharp;
+using TechTalk.SpecFlow;
+
+namespace ExchangeRateServiceTests.Steps
+{
+
+    [Binding]
+    public sealed class Base
+    {
+        private readonly BaseContext _baseContext;
+        private RestClient RestClient;
+        private const string url = "https://api.exchangeratesapi.io/latest";
+
+        public Base(BaseContext baseContext)
+        {
+            _baseContext = baseContext;
+        }
+
+        [Given(@"I created request with Url")]
+        public void GivenICreatedRequestWithUrl()
+        {
+            RestClient = new RestClient(url);
+        }
+
+        [Given(@"I executed the request")]
+        public void GivenIExecutedTheRequest()
+        {
+            var request = new RestRequest(Method.GET);
+            _baseContext.response = RestClient.Execute(request);
+        }
+    }
+}
--- NUnitTestProject1/Steps/CompareRequestUrl.cs
+++ NUnitTestProject1/Steps/CompareRequestUrl.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using NUnitTestProject1.Models;
-using RestSharp;
 using TechTalk.SpecFlow;
 
 namespace NUnitTestProject1.Steps
@@ -13,25 +8,11 @@ namespace NUnitTestProject1.Steps
     public sealed class CompareRequestUrl
     {
         private readonly BaseContext _baseContext;
-        private RestClient RestClient;
         private const string url = "https://api.exchangeratesapi.io/latest";
 
         public CompareRequestUrl(BaseContext baseContext)
         {
             _baseContext = baseContext;
-        }
-
-        [Given(@"I created request with Url")]
-        public void GivenICreatedRequestWithUrl()
-        {
-            RestClient = new RestClient(url);
-        }
-
-        [Given(@"I executed the request")]
-        public void GivenIExecutedTheRequest()
-        {
-            var request = new RestRequest(Method.GET);
-            _baseContext.response = RestClient.Execute(request);
         }
 
         [Then(@"I should have same url after execution")]
